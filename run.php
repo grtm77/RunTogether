4948e20bd095ef2c5443e786a2a76e633a04792c
grtm77 <50659884+grtm77@users.noreply.github.com>
2022-10-12 08:54:14 +0800

    Add files via upload

---
+++ run.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>RunTogether!</title>
+    <meta name="content-type" ; charset="UTF-8">
+</head>
+
+<body>
+    <div class="content" align="center">
+        <div class="header">
+            <h1><a style="font-size: 50px">RunTogether!</a></h1>
+            <h4></h4>
+            <h4></h4>
+        </div>
+        <div class="middle">
+            <form action="runtogether.php" method="post">
+                <table border="0" align="center">
+                    <tr>
+                        <td align="center">A组点位</td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="a0" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="a1" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="a2" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="a3" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="a4" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td align="center">B组点位</td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="b0" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="b1" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="b2" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="b3" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td><input type="text" name="b4" required="required"></td>
+                    </tr>
+                    <tr>
+                        <td align="center"><input type="submit"></td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+        <!--脚部-->
+        <div class="footer"> <small>Copyright &copy; Bultrail 版权所有 </div>
+    </div>
+</body>
+
+</html>
---
+++ runtogether/cmake-build-debug/CMakeFiles/runtogether.dir/depend 2.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for runtogether.
+# This may be replaced when dependencies are built.
